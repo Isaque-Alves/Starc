c80207839359710797e9f3c381f52b7a11a22a40
Breno <breno.rossi.L@hotmail.com>
2019-10-02 19:53:40 -0300

    Criação do App Mobile
    Criação do App Mobile

---
+++ STARC/STARC/STARC/PaginaPrincipal.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace STARC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaPrincipal : ContentPage
+    {
+        public PaginaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        async void Clicked_ImageAsync(object sender, EventArgs a)
+        {
+            Button b = (sender as Button);
+            b.Effects.Clear();
+
+            if(b.ImageSource.ToString() == "File: LampadaLigada.png")
+            {
+                b.ImageSource = "LampadaDesligada.png";
+            }else if (b.ImageSource.ToString() == "File: LampadaDesligada.png")
+            {
+                b.ImageSource = "LampadaLigada.png";
+            }
+
+            bool desabilitar = await DisplayAlert("Conflito", "O componente atual está em um grupo, deseja desativa-lo do grupo?", "Desabilitar", "Cancelar");
+
+               
+             
+        }
+    }
+}
