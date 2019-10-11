850c25ca5cdc906b41c30e8cd0ba352644fa390a
isaque <isaquea785@gmail.com>
2019-10-11 02:58:31 -0300

    Estilização login
    Apresenta erros no input da tela login no controller login

--- Star/Star/Migrations/AppContextModelSnapshot.cs
+++ Star/Star/Migrations/AppContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace Star.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062")
+                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("Star.Models.Cadastro", b =>
